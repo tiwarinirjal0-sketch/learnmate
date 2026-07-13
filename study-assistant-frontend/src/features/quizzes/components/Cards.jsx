bf1fd21f473726a1d72d3fc5e014e6af217be569
Nirjal <tiwarinirjal0@gmail.com>
2026-07-13 23:50:27 +0700

    add quiz card ui frontend architecture

--- study-assistant-frontend/src/features/quizzes/components/Cards.jsx
+++ study-assistant-frontend/src/features/quizzes/components/Cards.jsx
@@ -3,23 +3,21 @@ import { useState } from "react"
 
 
 
-export default function QuizCards(){
-    const quizzes = [
-        {qn:"what is your name", ans:"my name is nirjal"},
-        {qn:"age", ans:50}
-    ]
-    const [flip, setFlip] = useState(false)
+export default function QuizCards({qnIndex,quizzes}){
+    
+    
     return(
         <div className="relative h-80 w-72 bg-amber-50 flex items-center justify-center">
-           {quizzes.map(item=>(
+           
             <div 
-            onClick={()=>setFlip(prev=>!prev)}
+            
             className="absolute h-full w-full bg-amber-50 flex items-center justify-center">
-             {flip?item.ans:item.qn}
+             {quizzes[qnIndex].qn}
             </div>
             
            
-           ))}
+           
+           
         </div>
     )
 }
--- study-assistant-frontend/src/features/quizzes/pages/Quiz.jsx
+++ study-assistant-frontend/src/features/quizzes/pages/Quiz.jsx
@@ -1,10 +1,32 @@
 import QuizCards from "../components/Cards";
-
+import { useState } from "react";
 
 export default function Quiz(){
+    const [qnIndex, setQnIndex] = useState(0)
+    const [quizData, setQuizData] = useState([
+        {qn:"what is your name", ans:"my name is nirjal"},
+        {qn:"age", ans:50}
+    ])
     return (
-        <div className="min-h-screen min-w-screen items-center bg-blue-200 flex justify-center ">
-            <QuizCards />
+        <div className="flex gap-2 flex-col min-h-screen min-w-screen items-center bg-blue-200 flex justify-center ">
+
+            <QuizCards qnIndex={qnIndex} quizzes={quizData}/>
+             <div className="flex gap-4 ">
+               <button  
+                disabled={qnIndex===0}
+                className="bg-cyan-700 p-2 rounded-2xl hover:text-amber-50 disabled:bg-blue-400  disabled:cursor-not-allowed"
+                onClick={()=>setQnIndex(prev=>prev-1)}
+                >
+                 Prev
+               </button>
+              <button
+               disabled={qnIndex===quizData.length-1}
+                onClick={()=>setQnIndex(prev=>prev+1)}
+
+               className="bg-cyan-700 p-2 rounded-2xl  hover:text-amber-500 disabled:cursor-not-allowed" >
+                Next
+              </button>
+             </div>
         </div>
     )
 }
